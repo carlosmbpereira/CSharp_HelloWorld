83bb3c7d4dd3806f2a42a85f38f9c22e166f2dca
Serbap <Serbap@gmail.com>
2021-01-14 18:11:21 +0000

    esta é uma nova versao

--- CSharp_HelloWorld/CSharp_HelloWorld/App.xaml.cs
+++ CSharp_HelloWorld/CSharp_HelloWorld/App.xaml.cs
@@ -18,6 +18,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 //Sergio
+//qq coisa
+
 
 // The Grid App template is documented at http://go.microsoft.com/fwlink/?LinkId=234226
 
